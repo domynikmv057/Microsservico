21e1c8a3adf5751ac713fed79cf126c9ab77b3c8
domynikmv057 <domynikmv.057@gmail.com>
2021-08-16 15:56:58 -0300

    Implementação de um Microsserviço de um e-comerce

---
+++ product-catalog/src/main/java/one/digitalinnovation/experts/productcatalog/controller/ProductController.java
@@ -0,0 +1,28 @@
+package one.digitalinnovation.experts.productcatalog.controller;
+
+import one.digitalinnovation.experts.productcatalog.model.Product;
+import one.digitalinnovation.experts.productcatalog.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping(value = "/product")
+public class ProductController {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @RequestMapping(method = RequestMethod.POST)
+    Product create(@RequestBody Product product){
+
+       return productRepository.save(product);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    Optional<Product>  findById(@PathVariable Integer id){
+      return productRepository.findById(id);
+    }
+
+}
